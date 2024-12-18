e09db1882efc85bb7b04069075a048a163b1b92e
Yash Raj Suman <123866489+yashrajsuman@users.noreply.github.com>
2024-12-18 20:05:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,9 +16,19 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hi'); // Print "Hi" on the screen
+//const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`App running at http://localhost:${port}`);
 });
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, the database is connected!');
+});
+
+
 // Routes
 app.use('/api', loginRoutes);
 app.use('/api/performance', performanceRoutes);
